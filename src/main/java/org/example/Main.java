b318d0fc74f6a3b3db26e9630e5198881767da97
Jatin Mishra <jatinm1shra10cr@gmail.com>
2026-04-10 20:03:24 +0530

    adding chat ui generator'

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,3 +4,7 @@ public class Main {
     static void main() {
     }
 }
+
+/*
+8th Floor, Building known as ‘ECOWORLD – 4D, situated in Bellandur, Sarjapur Outer Ring Road, Bengaluru, Bengaluru, Karnataka, 560103, IN
+ */
